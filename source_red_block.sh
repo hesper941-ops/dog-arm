9213599924d021e95f5163eff4a53ea58650758b
hesper941-ops <246078243+hesper941-ops@users.noreply.github.com>
2026-05-02 04:27:02 +0800

    更新ROS2工作区环境脚本和忽略规则

--- source_red_block.sh
+++ source_red_block.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 source /opt/ros/humble/setup.bash
-source /home/sunrise/dog/ros2_red_block_ws/install/local_setup.bash
 
-export AMENT_PREFIX_PATH=/home/sunrise/dog/ros2_red_block_ws/install/red_block_grasp_ros2:$AMENT_PREFIX_PATH
-export CMAKE_PREFIX_PATH=/home/sunrise/dog/ros2_red_block_ws/install/red_block_grasp_ros2:$CMAKE_PREFIX_PATH
-export PATH=/home/sunrise/dog/ros2_red_block_ws/install/red_block_grasp_ros2/lib/red_block_grasp_ros2:$PATH
+WS=/home/sunrise/dog/ros2_red_block_ws
+PKG_PREFIX=$WS/install/red_block_grasp_ros2
+
+export AMENT_PREFIX_PATH=$PKG_PREFIX:${AMENT_PREFIX_PATH:-}
+export CMAKE_PREFIX_PATH=$PKG_PREFIX:${CMAKE_PREFIX_PATH:-}
+export PYTHONPATH=$PKG_PREFIX/lib/python3.10/site-packages:${PYTHONPATH:-}
+export PATH=$PKG_PREFIX/lib/red_block_grasp_ros2:${PATH:-}
